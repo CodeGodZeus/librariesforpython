375d41a6fed08e27fe28ffd467d81c9f4aa3894a
Kushagra <63976266+CodeGodZeus@users.noreply.github.com>
2020-09-30 01:33:15 +0530

    Update meanmedianmode.py

--- meanmedianmode.py
+++ meanmedianmode.py
@@ -67,3 +67,23 @@ plt.axvline(average_age, color='r', linestyle='solid', linewidth=2, label="Mean"
 plt.legend()
 
 plt.show()
+
+
+
+
+
+
+import numpy as np
+
+# Array of the first five author ages
+five_author_ages = np.array([29, 49, 42, 43, 32])
+
+# Fill in the empty array with the values sorted
+sorted_author_ages = np.array(sorted(five_author_ages))
+
+# Save the median value to median_value
+median_age = np.median(sorted_author_ages)
+
+# Print the sorted array and median value
+print("The sorted array is: " + str(sorted_author_ages))
+print("The median of the array is: " + str(median_age))
